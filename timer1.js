ab2a1d3e2e7216de8c63d961057d3a851b87c8d1
Cody <codylau6233@hotmail.com>
2023-11-04 23:25:44 -0700

    Add function timer to timer1.js file

--- timer1.js
+++ timer1.js
@@ -1,16 +1,32 @@
-// Implement an alarm clock / timer which will beep after a specified amount of time has passed.
-// The user can specify an unlimited number of alarms using command line arguments
-// process.stdout.write('\x07') beep sound
-const inputFromCI = process.argv.slice(2);
+const inputSeconds = process.argv.slice(2);
 
 const timer = (arrayOfSeconds) => {
-  for (const seconds of arrayOfSeconds) {
-    // edge cases
-    
+  
+  if (arrayOfSeconds.length === 0) {
+    console.log('invalid input: No time duration provided');
+    return;
+  }
+
+  // input arg is number but wrapped in ''
+  const secondsAsNumber = arrayOfSeconds.map((element) => Number(element));
+
+  for (const seconds of secondsAsNumber) {
+
+    if (isNaN(seconds)) {
+      console.log(`invalid input, input must be a number`);
+      continue;
+    }
+
+    if (seconds <= 0) {
+      console.log(`invalid input: ${seconds} , input must be a positive number`);
+      continue;
+    }
+
     setTimeout(() => {
       process.stdout.write('\x07');
     }, seconds * 1000);
+    
   }
 };
 
-timer(inputFromCI);
+timer(inputSeconds);
